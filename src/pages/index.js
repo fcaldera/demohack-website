b4565a4107b92177e6434d2ae5e22f965a63e3ee
Fernando Caldera <fernando@jobbatical.com>
2019-09-08 00:30:30 +0300

    Hack typography [WIP]

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,13 @@ import React from "react"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import { Text } from "rebass"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <Text color="text" fontFamily="body">Welcome to your new Gatsby site.</Text>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
