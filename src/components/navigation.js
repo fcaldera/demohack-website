0f885e59fe2f725d402dde0987c7b18e929fe249
Fernando Caldera <fernando@jobbatical.com>
2019-09-08 13:07:32 +0300

    Add navigation bar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,22 +1,42 @@
 import React from 'react';
-import { Flex, Box, Text, Button, Heading } from 'rebass';
-import { Link } from 'gatsby';
+import { Flex, Box, Text, Button, Link } from 'rebass';
+import { Link as GatsbyLink } from 'gatsby';
 import Container from './container';
 import styled from '@emotion/styled';
 
 const Brand = styled(Box)`
   font-size: 24px;
   font-weight: bold;
-  display: block;
   color: black;
   text-decoration: none;
 `;
 
 Brand.defaultProps = {
-  as: 'a'
+  as: 'a',
 };
 
+const NavLink = styled(Link)`
+  color: black;
+  font-size: 18px;
+  padding: 23px;
+  text-decoration: none;
+`;
 
+NavLink.defaultProps = {
+  as: GatsbyLink,
+};
+
+const NavButton = styled(Button)`
+  border-radius: 5px;
+  box-shadow: 0px 2px 2px rgba(176, 182, 193, 0.37);
+  font-size: 14px;
+  line-height: 1;
+  padding: 13px 16px;
+`;
+
+const SmallNavLink = styled(NavLink)`
+  font-size: 14px;
+`;
 
 const Navigation = () => (
   <Container>
@@ -25,17 +45,17 @@ const Navigation = () => (
         Crowdz.
       </Brand>
       <Box>
-        <Link to="/">How it works?</Link>
-        <Link to="/">Discover</Link>
-        <Link to="/">About</Link>
+        <NavLink to="/">How it works?</NavLink>
+        <NavLink to="/">Discover</NavLink>
+        <NavLink to="/">About</NavLink>
       </Box>
       <Box mx="auto" />
-      <Link to="/" variant="nav" href="#!">
-        Login
-      </Link>
-      <Button as={Link} to="/">
-        Join beta
-      </Button>
+      <Box>
+        <SmallNavLink>Login</SmallNavLink>
+        <NavButton as={Link} to="/" bg="black">
+          Join beta
+        </NavButton>
+      </Box>
     </Flex>
   </Container>
 );
