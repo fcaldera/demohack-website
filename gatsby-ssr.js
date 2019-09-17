32a3d3ed53a10cdcfdddd3d0084c7bc5c4bcf1ee
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-09-17 21:22:16 +0300

    fix: production build style

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,7 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react';
+import { ThemeProvider } from 'theme-ui';
+import theme from './src/theme';
 
-// You can delete this file if you're not using it
+export const wrapRootElement = ({ element }) => {
+  return <ThemeProvider theme={theme}>{element}</ThemeProvider>;
+};
